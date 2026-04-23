8acd55354578bebcb4350f6df15133c7756f7b75
Baria Harsh <harshbaria9662@gmail.com>
2026-04-23 22:19:11 +0530

    Add Capacitor Android support and Native App configuration

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.attendance.pro',
+  appName: 'Attendance Pro',
+  webDir: 'dist'
+};
+
+export default config;
